c860295f45b369b4e5e6b7c49370ef5e86b54f6e
Daniel Filho <df0l@outlook.com>
2022-02-09 10:58:10 -0300

    continuação do curso

---
+++ 010/index.js
@@ -0,0 +1,5 @@
+// NaN - Not a Number; ParseInt (inteiro); ParseFloat (flutuante);
+const num1 = 10
+const num2 = Number(Luiz)
+console.log(num1 + num2)
+console.log(typeof num2)
---
+++ 011/teste.js
@@ -0,0 +1,7 @@
+alert('Com a nossa mensagem.')
+const confirma = confirm('Deseja realmente apagar?')
+prompt('Digite seu nome, por favor')
+//EXERCÍCIO
+let num1 = parseFloat(prompt('Digite um número:'))
+let num2 = parseFloat(prompt('Digite outro número'))
+alert(`O resultlado da soma entre os 2 números digitados é: ${num1 + num2}`)
