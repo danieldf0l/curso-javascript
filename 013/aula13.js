67863b5c97c1ed672377c15e378e65655a610458
Daniel Filho <df0l@outlook.com>
2022-09-17 17:08:44 -0300

    atualização-notas

---
+++ 013/aula13.js
@@ -0,0 +1,9 @@
+             01234567
+let texto = 'Um texto'
+console.log(texto[4])
+console.log(texto.charAt(5))
+console.log(texto.concat(' em um lindo dia.'));
+console.log(texto + ' em um lindo dia.');
+console.log(`${texto} em um lindo dia`);
+console.log(texto.toUpperCase());
+console.log(texto.toLowerCase());
