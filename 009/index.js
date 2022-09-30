e239989f3413d3cfa5bc54deceaeaf43fac6901e
Daniel Filho <df0l@outlook.com>
2022-09-30 11:44:26 -0300

    correção

--- 009/index.js
+++ 009/index.js
@@ -2,7 +2,7 @@ const nome = 'Daniel'; //string
 const nome1 = "Daniel"; //string
 const nome2 = `Daniel`; //string
 const numero = 10; //Number
-const numero = 12.54; //Number
+const numero2 = 12.54; //Number
 
 let nomeUsuario;
 const sobrenomeUsuario = null;
